349b2f4c522c801773850cd06114fbba006a6c53
soham kedar <kedarsoham1808@gmail.com>
2026-06-15 15:05:31 +0530

    jgygy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { AuthProvider } from './context/AuthContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ThemeProvider>
         <AuthProvider>
           <App />
